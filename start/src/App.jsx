d6a8b9076ad668b2d3563aaae17dc1e92fe859a7
gaurav mandal <gauravmandal97255@gmail.com>
2025-04-25 22:37:18 +0530

    basic TAG

---
+++ start/src/App.jsx
@@ -0,0 +1,17 @@
+import Tag from "./assets/Tag"
+function App() {
+
+ return(
+  <>
+    <h1>Tag </h1>
+    <h1>Tag </h1>
+    <h1>Tag </h1>
+    <h1>Tag </h1>
+
+  </>
+  
+ )
+      
+}
+
+export default App
---
+++ start/src/assets/Tag.jsx
@@ -0,0 +1,7 @@
+ const Tag = function IamTag (){
+    return(
+      <h1>this is a tag</h1>
+    )
+ }
+
+ export default Tag
